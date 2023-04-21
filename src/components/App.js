af96969b9e17a98460108abf1e49ac38199332dd
simon <simonkhartoum@gmail.com>
2023-04-21 15:11:11 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header";
-import About from "./About";
-import ArticleList from "./ArticleList";
+import Header from "./header";
+import About from "./about";
+import ArticleList from "./articleList";
 
 console.log(blogData);
 
